d58fd5cccea2aece8b259af4dc3f99403df7dfa4
sherlock_lin <sherlock_lin@163.com>
2018-12-06 20:32:04 +0800

    add menu

--- scripts/index.js
+++ scripts/index.js
@@ -1,6 +1,25 @@
 var win = nw.Window.get();
-win.showDevTools();
-console.log(111);
-window.onload = function(){
-    console.log(222);
-}
+//win.showDevTools();
+
+var menu = new nw.Menu({type:'menubar'});//创建菜单条
+
+//添加菜单项
+menu.append(new nw.MenuItem({
+    label: 'Item A',
+    click: function(){
+        alert("Click item A");
+    }
+}));
+menu.append(new nw.MenuItem({
+    type: 'separator'
+}));
+menu.append(new nw.MenuItem({
+    label: 'Item B',
+    click: function(){
+        alert("Click item B");
+    }
+}));
+
+
+
+win.menu = menu;//将创建的菜单栏设置为窗口属性
